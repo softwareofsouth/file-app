0195c0b8e347884acaafaede508263dd87739153
dani54196 <dani54196@gmail.com>
2024-04-18 18:37:36 -0300

    readme

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 a site to uploda & view content, like photos & videos
 ### tools & framework
 node as backend, react as frontend
-
+multer
 ### upload in ec2 instance
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,36 +1,23 @@
-const express = require('express');
-const multer = require('multer');
-const cors = require('cors');
-const path = require('path');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const UploadRoute = require("./routes/UploadRoute");
 
 const app = express();
-const port = 3001;
-
 app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
 
-const storage = multer.diskStorage({
-  destination: './uploads/',
-  filename: function (req, file, cb) {
-    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-  }
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URI, () => {
+  console.log("MonoDB Connected...");
 });
 
-const upload = multer({ storage: storage }).single('file');
+app.use(UploadRoute);
 
-app.post('/upload', (req, res) => {
-  upload(req, res, (err) => {
-    if (err) {
-      console.error(err);
-      res.sendStatus(500);
-    } else {
-      const filePath = req.file.path;
-      res.json({ path: filePath });
-    }
-  });
-});
-
-app.use('/uploads', express.static('uploads'));
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server started at port: ${PORT}`);
 });
